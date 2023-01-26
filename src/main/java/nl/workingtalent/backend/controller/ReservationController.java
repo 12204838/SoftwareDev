ff612ec7072bf08f9f079b15200b07c431158afe
Just <just_plouvier@live.nl>
2023-01-26 17:02:52 +0100

    ReservationController updated

--- src/main/java/nl/workingtalent/backend/controller/ReservationController.java
+++ src/main/java/nl/workingtalent/backend/controller/ReservationController.java
@@ -1,9 +1,12 @@
 package nl.workingtalent.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import nl.workingtalent.backend.dto.ReservationDto;
 import nl.workingtalent.backend.dto.ResponseDto;
 import nl.workingtalent.backend.dto.UserDto;
+import nl.workingtalent.backend.entity.Book;
 import nl.workingtalent.backend.entity.Reservation;
 import nl.workingtalent.backend.entity.User;
 import nl.workingtalent.backend.repository.IReservationRepository;
@@ -34,4 +38,30 @@ public class ReservationController {
 		
 		return new ResponseDto();
 	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "reservations/update/{id}")
+	public ResponseDto updateReservationById(@PathVariable long id, @RequestBody Reservation reservation) {
+		Optional<Reservation> optional = reservationRepo.findById(id);
+		
+		if (optional.isEmpty()) {
+			return new ResponseDto("This book does not exist yet.");
+		}
+		
+		Reservation reservationDb = optional.get();
+		
+		reservationDb.setUser(reservation.getUser());
+		reservationDb.setBook(reservation.getBook());
+		reservationDb.setApproved(reservation.isApproved());
+		
+		reservationRepo.save(reservationDb);
+		
+		return new ResponseDto();		
+	}
+	
+	@DeleteMapping("reservations/delete/{id}")
+	public ResponseDto deleteReservationById(@PathVariable long id) {
+		reservationRepo.deleteById(id);
+		return new ResponseDto();	
+	}
+	
 }
