669f55931ac5bc2184d0fae0da3f907fa61f77c2
Martijn Hiemstra <info@greenhighway.nl>
2023-01-26 11:45:34 +0100

    Add bookdto

--- src/main/java/nl/workingtalent/backend/controller/BookController.java
+++ src/main/java/nl/workingtalent/backend/controller/BookController.java
@@ -1,11 +1,13 @@
 package nl.workingtalent.backend.controller;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.workingtalent.backend.dto.BookDto;
 import nl.workingtalent.backend.entity.Book;
 import nl.workingtalent.backend.repository.IBookRepository;
 
@@ -16,8 +18,11 @@ public class BookController {
 	private IBookRepository bookRepo;
 	
 	@RequestMapping("books")
-	public List<Book> books() {
-		return bookRepo.findAll();
+	public Stream<BookDto> books() {
+		List<Book> books = bookRepo.findAll();
+		
+		// Zet lijst van Book om naar lijst bookdto
+		return books.stream().map(book -> new BookDto(book));
 	}
 	
 }
---
+++ src/main/java/nl/workingtalent/backend/dto/BookDto.java
@@ -0,0 +1,72 @@
+package nl.workingtalent.backend.dto;
+
+import nl.workingtalent.backend.entity.Book;
+
+public class BookDto {
+
+	private long id;
+	
+	private String title;
+	
+	private String author;
+	
+	private String isbn;
+	
+	private int bookCopies;
+	
+	public BookDto() {
+		
+	}
+	
+	public BookDto(Book book) {
+		super();
+		this.id = book.getId();
+		this.title = book.getTitle();
+		this.author = book.getAuthor();
+		this.isbn = book.getIsbn();
+		this.bookCopies = book.getBookCopies().size();
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getBookCopies() {
+		return bookCopies;
+	}
+
+	public void setBookCopies(int bookCopies) {
+		this.bookCopies = bookCopies;
+	}
+	
+}
