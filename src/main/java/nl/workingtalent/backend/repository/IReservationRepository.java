afe2f63c6edf35ee2c82a7f396e926cf3929950f
12204838 <justinapps15@gmail.com>
2023-02-13 11:42:44 +0100

    added endpoint that returns list of reservations for speciifc logged in user

--- src/main/java/nl/workingtalent/backend/repository/IReservationRepository.java
+++ src/main/java/nl/workingtalent/backend/repository/IReservationRepository.java
@@ -1,9 +1,15 @@
 package nl.workingtalent.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import nl.workingtalent.backend.entity.Reservation;
+import nl.workingtalent.backend.entity.User;
 
 public interface IReservationRepository extends JpaRepository<Reservation, Long> {
+	
+	List<Reservation> findByUser(User user);
 
 }
